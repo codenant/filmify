be03cfa0b68a5cc11b1496e3c59b2246b433e594
codenant <e.g.dallo@gmail.com>
2024-09-25 04:32:56 +0400

    chore: add custom colors and font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mango: "#FCA311",
+        flatBlack: "#1F1F1F",
+        faded: "#242424",
+        flatWhite: "#F2F2F2",
+        mySlate: "#E5E5E5",
+        blink: "#1249C0",
+      },
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
