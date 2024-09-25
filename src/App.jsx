6b456bf76d62729ffd60b64368e477baeb683222
codenant <e.g.dallo@gmail.com>
2024-09-25 04:37:33 +0400

    feat: integrate header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Placeholder from "./pages/Placeholder";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+    // <Router>
+    //   <Routes>
+    //     <Route />
+    //   </Routes>
+    // </Router>
+  );
 }
 
 export default App;
