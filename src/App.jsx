294242c742d323513a1c3d11c417e7012a5a3007
codenant <e.g.dallo@gmail.com>
2024-09-26 11:41:27 +0400

    feat: integrate pages and components created so far using react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Placeholder from "./pages/Placeholder";
+import HomePage from "./pages/HomePage";
+import FilmDetails from "./pages/FilmDetails";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/details/:imdbID" element={<FilmDetails />} />
+      </Routes>
       <Footer />
-    </>
-    // <Router>
-    //   <Routes>
-    //     <Route />
-    //   </Routes>
-    // </Router>
+    </Router>
   );
 }
 
