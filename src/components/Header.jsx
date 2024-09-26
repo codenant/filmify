350fedd5569e667ddf99275730deb2cafd51c0e6
codenant <e.g.dallo@gmail.com>
2024-09-26 11:40:35 +0400

    feat: clear search bar when site logo is clicked to display landing page again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/filmify_original.png";
-import SearchBar from "./Searchbar";
+import SearchBar from "./SearchBar";
+import useFilmStore from "../stores/filmStore";
 
 function Header() {
+  // clear the search bar when the site logo is clicked
+  const setSearchInput = useFilmStore((state) => state.setSearchInput);
+  const handleClick = () => {
+    setSearchInput("");
+  };
+
   return (
     <div className="w-full h-[120px] bg-flatBlack flex flex-row justify-between">
       <div className="flex flex-row my-auto pl-8">
-        <img src={logo} alt="filmify logo" className="w-[120px] h-[45px]" />
+        <Link to="/">
+          <img
+            src={logo}
+            alt="filmify logo"
+            className="w-[120px] h-[45px]"
+            onClick={handleClick}
+          />
+        </Link>
       </div>
       <SearchBar />
       <div className="my-auto m-8">
