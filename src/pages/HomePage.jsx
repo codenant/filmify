523193b9c178c16a3f5db18e589cc732767e3417
codenant <e.g.dallo@gmail.com>
2024-09-28 00:14:47 +0400

    feat: add preloading, error handling, and integrate film card components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
 import { useEffect } from "react";
 import useFilmStore from "../stores/filmStore";
-import { Link } from "react-router-dom";
+import PreLoader from "./PreLoader";
+import FilmCard from "../components/FilmCard";
 
 function HomePage() {
-  let { displayFilms, fetchPopular, searchInput, fetchFilms, searchFilms } =
-    useFilmStore();
+  let {
+    displayFilms,
+    fetchPopular,
+    searchInput,
+    fetchFilms,
+    searchFilms,
+    isLoading,
+    isError,
+  } = useFilmStore();
 
   useEffect(() => {
     if (searchInput === "") {
@@ -17,31 +25,17 @@ function HomePage() {
   const display = searchInput === "" ? displayFilms : searchFilms; // ternary operator to change the array needed to map through based on if the search bar is empty or not
 
   return (
-    <div className="grid grid-cols-4 px-12 py-8 gap-8">
-      {display?.map((film) => (
-        <div
-          key={film.imdbID}
-          className="flex flex-col justify-center items-center"
-        >
-          <div>
-            <Link to={`/details/${film.imdbID}`}>
-              <img
-                src={film.Poster}
-                alt={film.Title}
-                className="w-[173px] h-[256px] shadow-md rounded-md hover:scale-105 transition duration-300 ease-in-out object-cover"
-              />
-            </Link>
-          </div>
-          <div className="w-[173px] mt-2">
-            <Link to={`/details/${film.imdbID}`}>
-              <h2 className="font-lato text-flatWhite font-semibold text-sm hover:text-mySlate hover:opacity-70 transition duration-200 ease-in-out">
-                {film.Title}
-              </h2>
-            </Link>
-            <p className="font-lato text-mySlate opacity-35">{film.Year}</p>
-          </div>
+    <div className="h-fit">
+      {isLoading ? (
+        <PreLoader />
+      ) : (
+        <div className="grid grid-cols-4 px-12 py-8 gap-8">
+          {display?.map((film) => (
+            <FilmCard film={film} />
+          ))}
         </div>
-      ))}
+      )}
+      {isError && <div className="errorMessage">{isError}</div>}
     </div>
   );
 }
