90e0f71793388021e61c6d2f88fa8d15c675d09b
codenant <e.g.dallo@gmail.com>
2024-09-28 00:15:20 +0400

    chore: integrate favorites page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import FilmDetails from "./pages/FilmDetails";
+import FavoritesPage from "./pages/FavoritesPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/details/:imdbID" element={<FilmDetails />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
       <Footer />
     </Router>
